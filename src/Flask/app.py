42a3837de1920b09cb9ca6ed43f5c0b826bdda30
Vamshipunna <40324102+Vamshipunna@users.noreply.github.com>
2022-10-24 12:27:26 +0530

    flaskapp added

--- src/Flask/app.py
+++ src/Flask/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
-    return "Hello...Welcome to Helm Demo.."
+@app.route('/')
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
 
 
 if __name__ == "__main__":
